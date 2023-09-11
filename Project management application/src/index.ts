825ae8b2767a81b1d80fcd658b6655ac30654584
NataliiaBoiko22 <nata.boyko.kras@gmail.com>
2023-09-11 21:54:30 +0200

    fix

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -6,7 +6,7 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://nataboykokras:qtV1xEoap0fZvxQB@cluster0.scg8pkw.mongodb.net/tests?retryWrites=true&w=majority');
+    await mongoose.connect('mongodb+srv://boikonataliia37:QBq9lSVVy0NAgkno@cluster0.ryxizcx.mongodb.net/test');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
